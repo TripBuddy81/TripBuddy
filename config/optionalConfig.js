4d6bc83fab225e27385f0f8587a6676e890d0ee0
TripBuddy81 <127129708+TripBuddy81@users.noreply.github.com>
2023-03-22 13:30:57 +0100

    Update optionalConfig.js

--- config/optionalConfig.js
+++ config/optionalConfig.js
@@ -1,3 +1,6 @@
+// Your personal settings go here!
+// Copy and modify any setting from config.js into this config file to overwrite any default settings. Insert sections if they are missing for you .e.g. "playlists"
+
 var optionalConfig = {
      'oAuthSpotify': [
         {
