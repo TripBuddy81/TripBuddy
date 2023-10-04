3f3cc3fe378355574d8e0174acdb093cf98e8738
Trip Buddy <TripBuddy81@gmail.com>
2023-10-04 19:42:38 +0200

    wip

--- js/main.js
+++ js/main.js
@@ -1250,6 +1250,7 @@ $( document ).ready( function () {
         } );
 
         $( '#spotifyPlaylists' ).click( function () {
+            spotifyInitOnPageLoad();
             $( '#spotifyPlaylistsMenu' ).toggle();
         } );
 
