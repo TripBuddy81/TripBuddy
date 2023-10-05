2217ccb4ecaf39278b0f3cb581b83a35e76ce9b9
TripBuddy81 <127129708+TripBuddy81@users.noreply.github.com>
2023-10-05 15:39:34 +0200

    Update optionalConfig.js

--- config/optionalConfig.js
+++ config/optionalConfig.js
@@ -2,46 +2,4 @@
 // Copy and modify any setting from config.js into this config file to overwrite any default settings. Insert sections if they are missing for you .e.g. "playlists"
 
 var optionalConfig = {
-     'oAuthSpotify': [
-        {
-            'redirect_uri' : 'http://127.0.0.1:8080/', // change to your actual value
-            'client_id'    : '',
-            'client_secret': ''
-        }
-    ],
-    'videosLocal': [
-        {
-            'videoLink': './media/videos/...',
-            'tags'     : ''
-        }
-    ],
-    'links'      : [
-        {
-            'link': 'https://youtube.com/playlist?list=PLcrVuDwOKu1eoYdyQxZAW7zA1gzO1GTEk',
-            'name': 'Psychedelic knowledge'
-        },
-        {
-            'link': 'https://www.youtube.com/@JacksonTyler/videos',
-            'name': 'Homemade Documentaries - Space'
-        },
-        {
-            'link': 'https://www.youtube.com/watch?v=M6M9-oFEKpk&list=PLl6BRvEJ-auZ5aYPHj1B3pKJ_pLjg9qNU',
-            'name': 'Luetin - Warhammer 40K Lore'
-        },
-        {
-            'link': 'https://www.youtube.com/watch?v=SNBVeR6m_Xk&list=PLzG6tj8mWFmAaPAujl6CSQMob_sW6L4yx',
-            'name': 'Psychedelic Short Films'
-        },
-        {
-            'link': 'https://www.youtube.com/watch?list=PL0DkyGTnL_T9PGRApBLu2iP9jMLSrFDYV&v=vlUR09yRHZU',
-            'name': 'Trippy Animations'
-        }
-    ],
-    'adultLinks' : [
-        {
-            'link': '',
-            'name': ''
-        }
-    ]
-
 }
