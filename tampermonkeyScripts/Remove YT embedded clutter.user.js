669795c73e45da9d295f98f1c0c1bbd2ab64a0a4
Trip Buddy <TripBuddy81@gmail.com>
2023-10-06 16:27:44 +0200

    wip

---
+++ tampermonkeyScripts/Remove YT embedded clutter.user.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         Remove YT embedded clutter
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.youtube.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=undefined.localhost
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+
+    removeFromIframe();
+    javascript:setInterval( removeFromIframe , 5000);
+
+    function removeFromIframe() {
+        try {
+            document.getElementsByClassName("ytp-button ytp-cards-button")[0].remove();
+        } catch(e) {
+        }
+
+        try {
+            document.getElementsByClassName("branding-img-container")[0].remove();
+        } catch(e) {
+        }
+
+        try {
+            document.getElementsByClassName("ytp-cards-teaser")[0].remove();
+        } catch(e) {
+        }
+
+        try {
+            document.getElementsByClassName("ytp-show-cards-title")[0].remove();
+        } catch(e) {
+        }
+
+        try {
+            document.getElementsByClassName("ytp-impression-link")[0].remove();
+        } catch(e) {
+        }
+
+    }
+
+
+})();
