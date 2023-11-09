6142b1bfe54f955a1b8669befb0e6fe82bef87d5
Trip Buddy <TripBuddy81@gmail.com>
2023-11-09 20:57:39 +0100

    wip

--- js/spotify.js
+++ js/spotify.js
@@ -315,7 +315,7 @@ function updateProgressBar( duration, progress, isPlaying ) {
         $( '#spotifyTrackProgress' ).show();
         percentageProgress = progress / duration;
         percentageRemaining = 1 - percentageProgress;
-        newWidth = 228 * percentageRemaining;
+        newWidth = ($( window ).width() - 30) * percentageRemaining;
         $( '#spotifyTrackProgress' ).attr( 'style', 'width:' + newWidth + 'px' );
     } else {
         $( '#spotifyTrackProgress' ).hide();
