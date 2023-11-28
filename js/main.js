c693502acff70869630f2d296681e213eadd6f86
Trip Buddy <TripBuddy81@gmail.com>
2023-11-28 12:31:16 +0100

    wip

--- js/main.js
+++ js/main.js
@@ -791,6 +791,17 @@ $( document ).ready( function () {
             }
 
             $( '.youtubeVideo' ).click( function ( event ) {
+                blockScreenSaver = true;
+                const container = $('#directYoutubePlayer')[0];
+                const fullscreenApi = container.requestFullscreen
+                        || container.webkitRequestFullScreen
+                        || container.mozRequestFullScreen
+                        || container.msRequestFullscreen;
+                fullscreenApi.call( container );
+
+                $( '.videoMenuOverlayFullscreen, .videoMenuOverlayFullscreen2' ).show();
+
+
                 youtubeVideoId = $( this ).attr( 'videoId' );
                 directYoutubePlayer.loadVideoById( youtubeVideoId );
                 directYoutubePlayer.playVideo();
