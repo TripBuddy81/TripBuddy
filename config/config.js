94edfb671bb07181f752eca66cf33d226f91b0c6
Trip Buddy <TripBuddy81@gmail.com>
2024-02-27 23:04:24 +0100

    wip

--- config/config.js
+++ config/config.js
@@ -116,6 +116,10 @@ var config = {
                         'label': 'Psybass',
                         'id'   : '0O1C7wbOthIxBbai9pYvEH'
                     },
+                    {
+                        'label': 'Psychamber',
+                        'id'   : '1z2HVIgmaCbPmro2oprLf5'
+                    }
                 ],
                 'blue' : [
                     {
@@ -162,10 +166,6 @@ var config = {
                         'label': 'Tool',
                         'id'   : '54c657PnbEgWUJt7biWHtO'
                     },
-                    {
-                        'label': 'Psychamber',
-                        'id'   : '1z2HVIgmaCbPmro2oprLf5'
-                    }
                 ],
                 'red'  : [
                     {
