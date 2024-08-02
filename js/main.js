a124729e2a32c704192b6e139c1322bc9b212a16
Trip Buddy <TripBuddy81@gmail.com>
2024-08-02 18:31:32 +0200

    wip

--- js/main.js
+++ js/main.js
@@ -4349,7 +4349,7 @@ $( document ).ready( function () {
 
             // For debug only
             if ( config['localSettingsOverwrite'] != undefined && config['localSettingsOverwrite']['debugMode'] != undefined && config['localSettingsOverwrite']['debugMode'] ) {
-                toggleXXXVisible();
+              /*  toggleXXXVisible();*/
 
                 /*   $( '#startMusicVideos' ).trigger( 'click' );*/
             }
