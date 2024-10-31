b37f62f7a5b00dcf8a57f63f4eb28c724aec1cb4
Trip Buddy <TripBuddy81@gmail.com>
2024-10-31 11:20:34 +0100

    wip

--- config/optionalConfig.js
+++
@@ -1,5 +0,0 @@
-// Your personal settings go here!
-// Copy and modify any setting from config.js into this config file to overwrite any default settings. Insert sections if they are missing for you .e.g. "playlists"
-
-var optionalConfig = {
-}
