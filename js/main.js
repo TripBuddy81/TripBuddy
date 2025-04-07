0169924c3e17feeefe81a14ed7a0fdd8d87696e3
Trip Buddy <TripBuddy81@gmail.com>
2025-04-07 12:09:26 +0200

    wip

--- js/main.js
+++ js/main.js
@@ -164,7 +164,7 @@ $( document ).ready( function () {
             window.directorModeShufflePropability = 33;
             window.directorModeTemporaryPauseInterval = '';
             window.directorModePlayHistory = {};
-            window.genericShortTermTempVariable = false;
+            window.trueMouseClickDetected = false;
 
             window.videodromeLoadModeMapping = {
                 '2': {
@@ -3823,22 +3823,26 @@ $( document ).ready( function () {
             } );
 
             $( document ).on( 'mousemove click', '.videodromeDirectorControlToBeHiddenIcon', function ( e ) {
-                genericShortTermTempVariable = false;
-                if ( e.type == 'click' ) {
-                    genericShortTermTempVariable = true;
+                trueMouseClickDetected = false;
+
+                if ( e.type == 'click' && e.originalEvent != undefined ) { // true click event detection
+                    trueMouseClickDetected = true;
                 }
                 clearTimeout( videodromeFullscreenMenuHideInterval );
                 videodromeFullscreenMenuHideInterval = setTimeout( function () {
                     $( '.videodromeDirectorControlToBeHiddenIcon' ).css( 'cursor', 'none' );
                     $( '.videodromeFullscreenMenuContainer' ).css( 'opacity', '0' );
-                    genericShortTermTempVariable = false;
+                    trueMouseClickDetected = false;
                 }, 1500 );
-                $( '.videodromeDirectorControlToBeHiddenIcon' ).css( 'cursor', 'url(assets/rainbow-gradient-pointer-32x32.png), auto' );
-                $( '#videodromeFullscreenMenuLocalVideoContainer,#videodromeFullscreenMenuVideoJSContainer' ).css( 'opacity', '1' );
+                if ( trueMouseClickDetected ) {
+                    $( '.videodromeDirectorControlToBeHiddenIcon' ).css( 'cursor', 'url(assets/rainbow-gradient-pointer-32x32.png), auto' );
+                    $( '#videodromeFullscreenMenuLocalVideoContainer,#videodromeFullscreenMenuVideoJSContainer' ).css( 'opacity', '1' );
+                }
+
             } );
 
             $( document ).on( 'mouseleave', '.videodromeDirectorControlToBeHiddenIcon', function () {
-                if ( !genericShortTermTempVariable ) {
+                if ( !trueMouseClickDetected ) {
                     clearTimeout( videodromeFullscreenMenuHideInterval );
                     $( '.videodromeDirectorControlToBeHiddenIcon' ).css( 'cursor', 'url(assets/rainbow-gradient-pointer-32x32.png), auto' );
                     $( '#videodromeFullscreenMenuLocalVideoContainer,#videodromeFullscreenMenuVideoJSContainer' ).css( 'opacity', '1' );
